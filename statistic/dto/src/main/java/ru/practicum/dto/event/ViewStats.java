dab7f34d49d79f9ad44507cbb9fa4c73b346a6e0
Andy Klim <43265590+Yasnodoom@users.noreply.github.com>
2024-08-22 16:38:03 +0300

    fist commit

---
+++ statistic/dto/src/main/java/ru/practicum/dto/event/ViewStats.java
@@ -0,0 +1,9 @@
+package ru.practicum.dto.event;
+
+public interface ViewStats {
+    String getApp();
+
+    String getUri();
+
+    Integer getHits();
+}
---
+++ statistic/server/src/main/java/ru/practicum/server/sevice/EventService.java
@@ -0,0 +1,33 @@
+package ru.practicum.server.sevice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.dto.event.Event;
+import ru.practicum.dto.event.ViewStats;
+import ru.practicum.server.storage.ServerRepository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final ServerRepository repository;
+
+    public Event save(Event data) {
+        return repository.save(data);
+    }
+
+    public List<ViewStats> findByParams(Timestamp start, Timestamp end, List<String> uris, boolean unique) {
+        if (unique) {
+            return repository.findByParamsUniqueIp(start, end, uris);
+        } else {
+            return repository.findByParams(start, end, uris);
+        }
+    }
+
+    public List<Event> findAll() {
+        return repository.findAll();
+    }
+
+}
